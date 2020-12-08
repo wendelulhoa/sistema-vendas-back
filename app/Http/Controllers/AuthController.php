aa646432c1853348caefb38430252f5ae206910a
wendelulhoa <wendelulhoa@gmail.com>
2020-12-08 10:04:27 -0300

    configuracao jwt

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('apiJwt', ['except' => ['login']]);
+    }
+    public function login(Request $request)
+    {
+        $email = $request['matricula'];
+        $password = $request['password'];
+        if (!$token = Auth::attempt(['matricula'=> $email, 'password'=> $password])) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function refresh()
+    {
+        return $this->respondWithToken(Auth::refresh());
+    }
+
+    public function me()
+    {
+      
+        return response()->json(auth()->user());
+    }
+
+    public function logout()
+    { 
+       Auth::logout();
+       return response()->json(['message' => 'Successfully logged out']);
+    }
+   
+    
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => Auth::factory()->getTTL() * 60,
+            'user'=> auth()->user()
+        ]);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,38 +6,26 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    public $timestamps = false;
     protected $fillable = [
-        'name',
-        'email',
+        'matricula',
         'password',
+        'adm',
     ];
+    public $hidden = ['password'];
+    protected $table = "tb_login";
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
