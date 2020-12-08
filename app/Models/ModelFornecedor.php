65cccc1e14a3e2c2a29a657b5918bf307e4dc70f
wendelulhoa <wendelulhoa@gmail.com>
2020-12-08 10:13:20 -0300

    criacao models

---
+++ app/Models/ModelFornecedor.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ModelFornecedor extends Model
+{
+    use HasFactory;
+}
